e5ff0826e3d36fd1a0ff1db81c10c610e7149e89
soroush <shuoros@yahoo.com>
2025-07-11 20:22:57 +0200

    INIT[spring boot app]

---
+++ src/main/java/com/scopevisio/praemiepro/PraemieProApplication.java
@@ -0,0 +1,13 @@
+package com.scopevisio.praemiepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraemieProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraemieProApplication.class, args);
+	}
+
+}
