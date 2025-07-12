c7b458b497fdca73f6e6870a3ad556ad87427a6d
soroush <shuoros@yahoo.com>
2025-07-12 13:43:26 +0200

    IMPLEMENT[user and authority entities and their liquibases with initial data]

---
+++ src/main/java/com/scopevisio/praemiepro/domain/AbstractTimestampedEntity.java
@@ -0,0 +1,44 @@
+package com.scopevisio.praemiepro.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdBy", "lastModifiedDate"}, allowGetters = true)
+public abstract class AbstractTimestampedEntity<T> implements Serializable {
+
+    public abstract T getId();
+
+    @CreatedDate
+    @Column(name = "created_date", updatable = false)
+    private Instant createdDate = Instant.now();
+
+    @LastModifiedDate
+    @Column(name = "last_modified_date")
+    private Instant lastModifiedDate = Instant.now();
+
+    public Instant getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Instant createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Instant getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Instant lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+}
